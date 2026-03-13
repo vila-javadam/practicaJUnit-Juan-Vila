a776cb3b86606126fb4345a408578463497e5362
JUAN <ejemplo@email.com>
2026-03-13 12:44:57 +0100

    Test: Verificar método setRadio()

--- CirculoTest.java
+++ CirculoTest.java
@@ -12,4 +12,10 @@ public class CirculoTest {
         Circulo c = new Circulo(5.0);
         assertEquals(5.0, c.getRadio(), 0.0);
     }
+    @Test
+    public void testSetRadio() {
+        Circulo c = new Circulo(2.0);
+        c.setRadio(7.5);
+        assertEquals(7.5, c.getRadio(), 0.0);
+    }
 }
