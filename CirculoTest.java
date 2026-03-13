9fb19284ccca74d7883cf04e14711229a02aa070
JUAN <ejemplo@email.com>
2026-03-13 12:41:38 +0100

    Test: Verificar constructor vacío (radio por defecto a 0 )

--- CirculoTest.java
+++ CirculoTest.java
@@ -2,5 +2,9 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class CirculoTest {
-
+    @Test
+    public void testConstructorVacio() {
+        Circulo c = new Circulo();
+        assertEquals(0, c.getRadio());
+    }
 }
